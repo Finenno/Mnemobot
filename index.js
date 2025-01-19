067c26bf50e45bb1574f81cac8cdb5c1ca651b5b
finenno <butfineno@gmail.com>
2025-01-19 20:35:27 +0300

    test commit

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const bot = new grammy.Bot(process.env.BOT_API_KEY);
 
 bot.on("message", async (ctx) => ctx.reply("В этом боте вы можете создавать свои карточки для запоминания терминов, новых слов на другом языке и чего бы то ни было другого, что выхотели бы выучить таким образом"));
 
-console.log("1234");
+console.log("12334");
+
 bot.start();
