99c13023ef5aea67fe62b7af26866e9595791cce
sni4 <artemhonest@gmail.com>
2025-01-29 00:09:01 +0300

    monitoring server, start function

--- modules/misc/botStart.js
+++ modules/misc/botStart.js
@@ -1,13 +1,17 @@
-/*
-const { bot } = require("../../index");
+const pool = require("../../db");
 
 
-async function botStart() {
+async function botStart(bot, sendMessagesToAdmins) {
   console.log('Bot started!');
   try {
-    // Уведомление о запуске бота
-    await bot.api.sendMessage(622655681, 'Bot started!');
-    await bot.api.sendMessage(556690739, 'Bot started!');
+    if (sendMessagesToAdmins === true){
+      await bot.api.sendMessage(622655681, 'Bot started!', {
+        disable_notification: true
+      });
+      await bot.api.sendMessage(556690739, 'Bot started!', {
+        disable_notification: true
+    })};
+
   } catch (err) {
     console.error('Failed to send startup message:', err);
   }
@@ -16,20 +20,28 @@ async function botStart() {
     // Подключение к базе данных
     const client = await pool.connect(); // Установить соединение
     console.log('Database connection is established!');
-    
-    await bot.api.sendMessage(622655681, 'Database connection is established!');
-    await bot.api.sendMessage(556690739, 'Database connection is established!');
+    if (sendMessagesToAdmins === true){
+      await bot.api.sendMessage(622655681, 'Database connection is established!', {
+        disable_notification: true
+      });
+      await bot.api.sendMessage(556690739, 'Database connection is established!', {
+        disable_notification: true
+    })};
 
     client.release(); // Освобождаем соединение
   } catch (err) {
     console.error('Database connection error:', err);
 
-    // Уведомление об ошибке подключения
     const errorMessage = `Database connection error: ${err.message}`;
-    await bot.api.sendMessage(622655681, errorMessage);
-    await bot.api.sendMessage(556690739, errorMessage);
+    if (sendMessagesToAdmins === true){
+      await bot.api.sendMessage(622655681, errorMessage, {
+        disable_notification: true
+      });
+      await bot.api.sendMessage(556690739, errorMessage, {
+        disable_notification: true
+    })};
+
   }
 };
 
 module.exports = { botStart };
-*/
--- modules/misc/serverStats.js
+++ modules/misc/serverStats.js
@@ -1,47 +1,25 @@
-// const os = require("os");
+const os = require("os");
 
 
-// function getServerStats() {
-//     try {
-//         const loadAverage = os.loadavg();
-  
-//         const totalMemory = os.totalmem();
-//         const freeMemory = os.freemem();
-//         const usedMemory = ((totalMemory - freeMemory) / 1024 / 1024).toFixed(2);
-//         const uptime = (os.uptime() / 3600).toFixed(2);;
+function getServerStats() {
+    try {
+        const loadAverage = os.loadavg();
+        const totalMemory = os.totalmem();
+        const freeMemory = os.freemem();
+        const usedMemory = ((totalMemory - freeMemory) / 1024 / 1024).toFixed(2);
+        const uptime = (os.uptime() / 3600).toFixed(2);;
 
-//         let text = `
-//         Memory: ${usedMemory} MB / ${(totalMemory / 1024 / 1024).toFixed(2)} MB
-//         CPU (1, 5, 15 mins average load): ${loadAverage} 
-//         Uptime: ${uptime} hours
-//         `
-//         console.log(text)
-//         return text
+        let text = `
+        Memory: ${usedMemory} MB / ${(totalMemory / 1024 / 1024).toFixed(2)} MB 
+CPU (1, 5, 15 mins average load): ${loadAverage} 
+Uptime: ${uptime} hours | ${(uptime/24).toFixed(0)} days ${(uptime%24).toFixed(0)} hours
+        `
+        
+        return text
     
-//     } catch (err) {
-//         return err;
-//     }
-// };
+    } catch (err) {
+        return err;
+    }
+};
 
-// function monitorServer() {
-//     const loadAverage = os.loadavg()[0]; 
-//     const cpuCount = os.cpus().length; 
-//     const cpuLoadPercent = ((loadAverage / cpuCount) * 100).toFixed(2);
-
-//     const totalMemory = os.totalmem();
-//     const freeMemory = os.freemem();
-//     const memoryUsage = (((totalMemory - freeMemory) / totalMemory) * 100).toFixed(2);
-
-//     if (cpuLoadPercent > CPU_THRESHOLD) {
-//         console.log(`⚠️ CPU is overloaded: ${cpuLoadPercent}%`);
-//     }
-
-//     if (memoryUsage > MEMORY_THRESHOLD) {
-//         console.log(`⚠️ Memory usage is high: ${memoryUsage}%`);
-//     }
-// };
-
-// getServerStats();
-
-
-// module.exports = { getServerStats, monitorServer };
+module.exports = { getServerStats };
